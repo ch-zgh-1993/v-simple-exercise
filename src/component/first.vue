46d0cc879ef2b7becb795cefad065a760769989d
zhangguohua <461974918@qq.com>
2018-07-22 15:11:53 +0800

    add content

---
+++ src/component/first.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="first">
+        <em>{{ msg }}</em>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'first',
+        data () {
+            return {
+                msg: 'hello, everything will be good!'
+            }
+        }
+    }
+</script>
+
+<style>
+    em {
+        color: orange;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import First from './component/first.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(First)
+});
